b79a5b0a48d96181bfe837110955ea5ad127ffd7
whosubashsubedii <subedisubash@protonmail.com>
2025-06-22 23:02:22 +0545

    a0

--- missed_ae_in_juniper.py
+++ missed_ae_in_juniper.py
@@ -1,22 +1,20 @@
-vpls_name = ("Enter the name of vpls: ")
-aggregated_ethernet = input {"Enter the Aggregated Ethernet"}
-vlan = input ("Enter the Vlan Number")
+vpls_name = input("Enter the name of VPLS: ")
+aggregated_ethernet = input("Enter the Aggregated Ethernet number (e.g., 16): ")
+vlan = input("Enter the VLAN Number: ")
 
-
-print("""
+print(f"""
 
 ### Command to Configure a Missing Aggregated Ethernet (AE) Interface in Juniper Router  ####
 
-
 configure private
 
-set interfaces ae{aggregated_ethernet} unit {vlan} description  {vpls_name}
+set interfaces ae{aggregated_ethernet} unit {vlan} description {vpls_name}
 set interfaces ae{aggregated_ethernet} unit {vlan} encapsulation vlan-vpls
-set interfaces ae{aggregated_ethernet} unit {vlan} vlan-id  {vlan}
+set interfaces ae{aggregated_ethernet} unit {vlan} vlan-id {vlan}
 set interfaces ae{aggregated_ethernet} unit {vlan} family vpls
 set routing-instances {vpls_name} interface ae{aggregated_ethernet}.{vlan}
-set routing-instances {vpls_name} protocols vpls vpls-id  {vlan}
-set routing-instances {vpls_name} description {vpls_name}
+set routing-instances {vpls_name} protocols vpls vpls-id {vlan}
+set routing-instances {vpls_name} description "{vpls_name}"
 set routing-instances {vpls_name} instance-type vpls
 set routing-instances {vpls_name} protocols vpls no-tunnel-services
 set routing-instances {vpls_name} protocols vpls vpls-id {vlan}
@@ -25,4 +23,4 @@ commit check
 
 """)
 
-a = input()
+input("Press Enter to exit...")
